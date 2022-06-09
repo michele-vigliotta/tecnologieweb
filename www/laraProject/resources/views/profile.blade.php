5e7985bf2f31557ab2d1c0dacf678d1f44e6b01a
Diego30090 <diego.vaccarini@gmail.com>
2022-06-09 18:10:24 +0200

    hidden admin useless info

--- www/laraProject/resources/views/profile.blade.php
+++ www/laraProject/resources/views/profile.blade.php
@@ -34,8 +34,9 @@
                               <div class="d-none d-md-block col-md-9">
    <div class="d-flex align-items-center justify-content-between">
       <h3 class="h2 mr-auto">{{Auth::user()->username}}
-
+          @if('admin'!==(Auth::user()->tipo))
           <a href="{{ route('profileedit')}}" style="font-size:16px;">&nbsp;Modifica il profilo</a></h3>
+       @endif
    </div>
    <div class="account-info">
       <div class="row py-2">
@@ -43,21 +44,26 @@
          <div class="col-12 col-md-8 col-xl-9 dd"><span>{{Auth::user()->nome}}&nbsp;{{Auth::user()->cognome}}</span></div>
       </div>
        <hr>
+       @if('admin'!==(Auth::user()->tipo))
+           <div class="row py-2">
+             <div class="col-12 col-md-4 col-xl-3 mb-2 mb-md-0 dt">Codice Fiscale</div>
+             <div class="col-12 col-md-8 col-xl-9 dd">{{Auth::user()->c_fiscale}}</div>
+           </div>
+           <hr>
+       @endif
        <div class="row py-2">
-         <div class="col-12 col-md-4 col-xl-3 mb-2 mb-md-0 dt">Codice Fiscale</div>
-         <div class="col-12 col-md-8 col-xl-9 dd">{{Auth::user()->c_fiscale}}</div>
-      </div>
+           <div class="col-12 col-md-4 col-xl-3 mb-2 mb-md-0 dt">Tipo di account</div>
+           <div class="col-12 col-md-8 col-xl-9 dd"><span style="display: inline-block; width: 16px; height: 16px; background-image: url('//static.cardmarket.com/img/f8d806e6267c7c859592cba11335f3d6/spriteSheets/ssMain2.png'); background-position: -448px -16px;" title="" data-toggle="tooltip" data-html="true" data-placement="bottom" onmouseover="showMsgBox(this,`Privato`)" onmouseout="hideMsgBox()" data-original-title="Privato" class="icon"></span><span class="ml-2">{{Auth::user()->tipo}}</span></div>
+       </div>
        <hr>
-      <div class="row py-2">
-         <div class="col-12 col-md-4 col-xl-3 mb-2 mb-md-0 dt">Tipo di account</div>
-         <div class="col-12 col-md-8 col-xl-9 dd"><span style="display: inline-block; width: 16px; height: 16px; background-image: url('//static.cardmarket.com/img/f8d806e6267c7c859592cba11335f3d6/spriteSheets/ssMain2.png'); background-position: -448px -16px;" title="" data-toggle="tooltip" data-html="true" data-placement="bottom" onmouseover="showMsgBox(this,`Privato`)" onmouseout="hideMsgBox()" data-original-title="Privato" class="icon"></span><span class="ml-2">{{Auth::user()->tipo}}</span></div>
-      </div>
-       <hr>
-      <div class="row py-2">
-         <div class="col-12 col-md-4 col-xl-3 mb-2 mb-md-0 dt">Data di nascita</div>
-         <div class="col-12 col-md-8 col-xl-9 dd">{{date('d-m-Y', strtotime(Auth::user()->data_nascita))}}</div>
-      </div>
-       <hr>
+       @if('admin'!==(Auth::user()->tipo))
+          <div class="row py-2">
+             <div class="col-12 col-md-4 col-xl-3 mb-2 mb-md-0 dt">Data di nascita</div>
+             <div class="col-12 col-md-8 col-xl-9 dd">{{date('d-m-Y', strtotime(Auth::user()->data_nascita))}}</div>
+
+          </div>
+           <hr>
+
        <div class="row py-2">
          <div class="col-12 col-md-4 col-xl-3 mb-2 mb-md-0 dt">Email</div>
          <div class="col-12 col-md-8 col-xl-9 dd">{{Auth::user()->email}}</div>
@@ -67,6 +73,7 @@
          <div class="col-12 col-md-4 col-xl-3 mb-2 mb-md-0 dt">Numero</div>
          <div class="col-12 col-md-8 col-xl-9 dd">+{{Auth::user()->prefisso}}&nbsp;{{Auth::user()->numero}}</div>
       </div>
+       @endif
 </div>
 
 
